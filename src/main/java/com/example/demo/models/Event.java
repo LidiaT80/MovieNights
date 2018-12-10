913312328051d82aa42c894b28942f32f284851d
LidiaT <tothlidia80@gmail.com>
2018-12-10 16:00:04 +0100

    created Event class

---
+++ src/main/java/com/example/demo/models/Event.java
@@ -0,0 +1,39 @@
+package com.example.demo.models;
+
+import com.google.api.client.util.DateTime;
+
+public class Event {
+    private String title;
+    private DateTime start;
+    private DateTime end;
+
+    public Event(String title, DateTime start, DateTime end){
+        this.title = title;
+        this.start = start;
+        this.end = end;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public DateTime getStart() {
+        return start;
+    }
+
+    public void setStart(DateTime start) {
+        this.start = start;
+    }
+
+    public DateTime getEnd() {
+        return end;
+    }
+
+    public void setEnd(DateTime end) {
+        this.end = end;
+    }
+}
